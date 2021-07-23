80c1eaa50f40f2177abd0f298ec8ab8764e8fcab
wpelliss <wilfried.pellissard@hotmail.fr>
2021-07-23 14:32:40 +0200

    Correctif NAN

--- content.js
+++ content.js
@@ -25,6 +25,7 @@ window.onload = function () {
 				note = note.innerHTML;
 			var num = page[i].getElementsByClassName(path1)[0].innerHTML;
 			num = num.indexOf('&nbsp;') >= 0 ? num.split('&nbsp;').join('') : num;
+			num = num.replace(/\D/g,'');
 			var score = ((note[0] + note[2]) - 44) * num;
 			if (score > 0) {
 				liste.push(score);
@@ -39,23 +40,37 @@ window.onload = function () {
 				note = note.innerHTML;
 			var num = page[i].getElementsByClassName(path1)[0].innerHTML;
 			num = num.indexOf('&nbsp;') >= 0 ? num.split('&nbsp;').join('') : num;
+			num = num.replace(/\D/g,'');
 			var score = ((note[0] + note[2]) - 44) * num;
+			
+			var color = "Grey";
+			var message = "";
+			
+			if (score > 0)
+			{
+				color = "Black";
+			}
+			if (score >= Math.max.apply(Math, liste) / 4)
+			{
+				color = "Blue";
+				message = "Convenable";
+			}
+			if (score >= Math.max.apply(Math, liste) / 2)
+			{
+				color = "Green";
+				message = "Bonne offre";
+			}
+			if (score == Math.max.apply(Math, liste))
+			{
+				color = "Red";
+				message = "MEILLEURE OFFRE";
+			}
 
-			if (score == Math.max.apply(Math, liste)) {
-				if (path == "s") {
-					document.getElementsByClassName("s-result-list")[0].getElementsByClassName("s-result-item")[i].getElementsByClassName("a-size-base")[0].parentNode.parentNode.innerHTML += "<br/><b><h1 style=\"color:Red;\"> MEILLEURE OFFRE (" + score + ") </h1></b>";
-				}
-				if (path == "gp") {
-					document.getElementById("zg-ordered-list").getElementsByClassName("a-list-item")[i].getElementsByClassName("a-size-small a-link-normal")[0].parentNode.parentNode.innerHTML += "<b><h1 style=\"color:Red;\"> MEILLEURE OFFRE (" + score + ") </h1></b><br/>";
-				}
-			} else {
-				if (path == "s") {
-					document.getElementsByClassName("s-result-list")[0].getElementsByClassName("s-result-item")[i].getElementsByClassName("a-size-base")[0].parentNode.parentNode.innerHTML += "<br/><b> Score (" + score + ")</b>";
-				}
-				if (path == "gp") {
-					document.getElementById("zg-ordered-list").getElementsByClassName("a-list-item")[i].getElementsByClassName("a-size-small a-link-normal")[0].parentNode.parentNode.innerHTML += "<b> Score (" + score + ")</b>";
-				}
-
+			if (path == "s") {
+				document.getElementsByClassName("s-result-list")[0].getElementsByClassName("s-result-item")[i].getElementsByClassName("a-size-base")[0].parentNode.parentNode.innerHTML += "<br/><b><div style=\"color:" + color + ";\"> " + message + " (" + score + ") </div></b>";
+			}
+			if (path == "gp") {
+				document.getElementById("zg-ordered-list").getElementsByClassName("a-list-item")[i].getElementsByClassName("a-size-small a-link-normal")[0].parentNode.parentNode.innerHTML += "<b><div style=\"color:" + color + ";\"> " + message + " (" + score + ") </div></b><br/>";
 			}
 		}
 	}
